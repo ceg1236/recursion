09408ac2437935b1a0498bdb390df00356babdb8
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-08-17 21:51:26 -0700

    "Initial attempts, some ideas"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,31 +6,41 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  //console.log('answer', document.getElementsByClassName(className));
+  var nodes = document.body,
+      matches = [];
 
-  var result,
-      nodes = document.body;
-// Run thru body, if childNode has className, push to result; 
+
+// Run thru body, if childNode has className, push chlildNode to result; 
 // if childNode has childNode, recurse as if childNode is body
 // this will require an inner-fn that takes the elmt to run-thru as the arg (?)
-	var walk = function(elmt) {
-		var matches = [];
+/*	var walk = function(elmt) {
+
 		if($(elmt).hasClass(className)) {
 			console.log("hasClass", $(elmt).hasClass(className));
 			matches.push(elmt);
 
 	       console.log('elmt', elmt);
 	       console.log('next',elmt.nextSibling);
-			  if($(elmt).nextSibling) {
-			  	console.log('3rd');
-			  	walk(elmt.nextSibling);
+			  if(elmt.nextSibling) {
+			  	console.log('sibling');
+			  	if($(elmt).hasClass(className)) {
+					matches.push(elmt.nextSibling);
+				}
+			  	
 			  } else if (elmt.childNodes) {
 			  	var childs = elmt.childNodes;
 			  	console.log('kids',childs);
-			  	
-			  	console.log('tag?', elmt.outerHTML);
-			  	walk(elmt.outerHTML);
+
+			  	for(var i=0; i<childs.length; i++) {
+			  		if($(childs[i]).hasClass(className)) {
+			  			console.log('again!');
+			  			matches.push(childs[i]);
+			  		}
+			  	}
+			  	console.log('list',elmt.classList );
 			  }
-		}
+		} else{console.log('NOPE');}
 		console.log('fin', matches);
 		return matches; 
 	};
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,20 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  console.log('type:', typeof obj);
+
+  if(typeof obj === 'object'){	
+    var elmt = '{'+obj+'}';
+    console.log('elmt:', elmt,'length:', elmt.length, 'obj:', obj);
+    return (elmt.indexOf('[')>0? '[' +obj+']':
+    		elmt.slice(elmt.indexOf('{')+1, elmt.indexOf('}'))); 
+
+  } else if (typeof obj === 'string'){
+  	console.log('test', obj);
+  	return "\""+obj+"\"";
+
+  } else {
+  return obj.toString(); 
+  }
+ 
 };
