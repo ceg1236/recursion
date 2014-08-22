c723d57f9a556dac81581bdeb9237fdd7532cfd5
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-08-22 00:24:31 -0700

    "Making headway, using recursion, bit sloppy"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,20 +4,54 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
-  console.log('type:', typeof obj);
+  //Ideas: Array.isArray(obj)        '{'+obj+'}'
+  //    elmt.slice(elmt.indexOf('{')+1, elmt.indexOf('}'))
 
-  if(typeof obj === 'object'){	
-    var elmt = '{'+obj+'}';
-    console.log('elmt:', elmt,'length:', elmt.length, 'obj:', obj);
-    return (elmt.indexOf('[')>0? '[' +obj+']':
-    		elmt.slice(elmt.indexOf('{')+1, elmt.indexOf('}'))); 
+  console.log('obj', typeof obj, obj);
 
-  } else if (typeof obj === 'string'){
-  	console.log('test', obj);
-  	return "\""+obj+"\"";
-
-  } else {
-  return obj.toString(); 
+  if(typeof obj === 'number') {
+    return obj.toString(); 
   }
- 
+  if(typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if(typeof obj === 'string') {
+    //obj = obj.toString();
+    obj = '\"'+obj+'\"';
+
+  }
+  if(typeof obj === 'object') {  
+    if(obj === null) {
+      obj = 'null';
+    }
+    if(Array.isArray(obj)) {
+      if(obj.length===0) {
+        obj = "[]";
+        return obj; 
+      }
+      console.log('length',obj.length);
+      if(obj.length>1) {
+          var array = []; 
+          for(var i = 0; i<obj.length; i++) {
+             array.push(stringifyJSON(obj[i]));
+             array.join(',');
+            }
+            return '['+array+']';
+       
+      }
+      else if(obj.length===1){
+        stringifyJSON(obj[0]);
+
+       if(typeof obj[0] === 'number') {
+          return '['+obj+']';
+        } else if(typeof obj[0] === 'string') {
+          return '[\"'+ obj[0]+'\"]'; 
+        } else if(typeof obj[0] === 'object') {
+          console.log('OOBB');
+          stringifyJSON(obj[0]);
+        }
+      } 
+    }
+  }
+return obj; 
 };
